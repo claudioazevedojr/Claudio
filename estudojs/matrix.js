1e2d6cdb73fb7a42b04537a4c9ad5d8e30c05fcf
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-09-07 20:24:51 -0300

    Create matrix.js

---
+++ estudojs/matrix.js
@@ -0,0 +1,20 @@
+const animais = [
+    ["gato", "Felino", 5],
+    ["cachorro", "Mamífero", 10],
+    ["coelho", "Mamífero", 7],
+    ["Tigre", "Felino", 4],
+    ["vaca", "Mamífero", 6]
+  ];
+
+  for (let i = 0; i < animais.length; i++) {
+    const animal = animais[i];
+    const nome = animal[0];
+    const especie = animal[1];
+    const idade = animal[2];
+  
+    console.log(`Animal ${i+1}:`);
+    console.log(`Nome: ${nome}`);
+    console.log(`Espécie: ${especie}`);
+    console.log(`Idade: ${idade}`);
+    console.log("--------------");
+  }
