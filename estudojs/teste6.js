1fdbe932efd8ae658bbd7c7205d7c933e427d79f
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-09-11 15:41:42 -0300

    Create teste6.js

---
+++ estudojs/teste6.js
@@ -0,0 +1,57 @@
+const prompt= require('prompt-sync')()
+
+op=prompt("Deseja continuar? SIM ou NÃO: ").toUpperCase();
+
+function receberInformacoes(nomes = [],idades = [], coresFavoritas = []) {
+    
+    while (op==="SIM") {
+        var nome = prompt("Digite o nome da pessoa " + (i + 1) + ":");
+        nomes.push(nome);
+    
+        var idade = parseInt(prompt("Digite a idade de " + nome + ":"));
+        idades.push(idade);
+    
+        var corFavorita = prompt("Digite a cor favorita de " + nome + ":");
+        coresFavoritas.push(corFavorita);
+      }
+    
+      return {
+        nomes: nomes,
+        idades: idades,
+        coresFavoritas: coresFavoritas
+      }; 
+      
+     
+
+    }
+    
+  
+function imprimirInformacoes(nomes, idades, coresFavoritas) {
+    console.log("Lista de Nomes:", nomes);
+    console.log("Lista de Idades:", idades);
+    console.log("Lista de Cores Favoritas:", coresFavoritas);
+  }
+  
+  // Recebendo informações do usuário
+  var informacoes = receberInformacoes();
+  
+  // Imprimindo informações originais
+  console.log("Informações originais:");
+  imprimirInformacoes(informacoes.nomes, informacoes.idades, informacoes.coresFavoritas);
+  
+  // Modificando uma cor e uma idade
+  var indiceModificar = parseInt(prompt("Digite o índice da pessoa(de 0 a 9):"));
+  if (indiceModificar >= 0 && indiceModificar < 10) {
+    var novaIdade = parseInt(prompt("Digite a nova idade para " + informacoes.nomes[indiceModificar] + ":"));
+    var novaCor = prompt("Digite a nova cor favorita para " + informacoes.nomes[indiceModificar] + ":");
+    
+    informacoes.idades[indiceModificar] = novaIdade;
+    informacoes.coresFavoritas[indiceModificar] = novaCor;
+  } else {
+    console.log("Índice inválido.");
+  }
+  
+  // Imprimindo informações após as modificações
+  console.log("Informações após modificações:");
+  imprimirInformacoes(informacoes.nomes, informacoes.idades, informacoes.coresFavoritas);
+  
