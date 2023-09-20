bb8895488c4bb7f10e83d3b9bc8f4d79fdbe5f67
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-09-20 20:21:55 -0300

    Create agencia.js

---
+++ estudojs/bancoNode/model/agencia.js
@@ -0,0 +1,9 @@
+export class agencia{
+    constructor(nome, numero, telefone, email, endere){
+        this.nome = nome;
+        this.numero = numero;
+        this.telefone = telefone;
+        this.email = email;
+        this.endere = endere;
+    }
+}
