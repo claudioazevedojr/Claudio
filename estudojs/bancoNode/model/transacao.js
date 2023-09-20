ec00c63b031ab767f3689fcc3bdfd82712b92a0d
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-09-20 20:31:44 -0300

    Create transacao.js

---
+++ estudojs/bancoNode/model/transacao.js
@@ -0,0 +1,10 @@
+export class transacao{
+    constructor(tipo, data, valor, clienteTransferencia, tranferencia, tipovalor){
+        this.tipo = tipo;
+        this.data = data;
+        this.valor = valor;
+        this.clienteTransferencia = clienteTransferencia;
+        this.tranferencia = tranferencia;
+        this.tipovalor = tipovalor;
+    }
+}
