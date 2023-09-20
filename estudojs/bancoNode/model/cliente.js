3cc20c09a4910746f80536fd9d5baf3be0656003
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-09-20 20:21:58 -0300

    Create cliente.js

---
+++ estudojs/bancoNode/model/cliente.js
@@ -0,0 +1,11 @@
+export class cliente {
+    constructor(nome, cpf, datanascimento, rg, email, telefone, end) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.datanascimento = datanascimento;
+        this.rg = rg;
+        this.email = email;
+        this.telefone = telefone;
+        this.end = end;
+    }
+}
