dfea18c9ce843ddfece8f9fad8e07610a6ef1ded
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-09-20 20:22:05 -0300

    Create endereco.js

---
+++ estudojs/bancoNode/model/endereco.js
@@ -0,0 +1,11 @@
+export class endereco {
+    constructor(uf, cidade, bairro, rua, numero, comp, cep){
+    this.uf = uf;
+    this.cidade = cidade;
+    this.bairro = bairro;
+    this.rua = rua;
+    this.numero = numero;
+    this.comp  = comp;
+    this.cep = cep;
+    }
+}
