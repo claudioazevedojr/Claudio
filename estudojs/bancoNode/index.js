6b49ee95523aef9e457090401ac2ccf64ef7f2fa
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-09-20 21:33:37 -0300

    Update index.js

--- estudojs/bancoNode/index.js
+++ estudojs/bancoNode/index.js
@@ -1,2 +1,8 @@
-console.log("Olá mundo!");
+import { endereco } from "./model/endereco.js";
+import { agencia } from "./model/agencia.js";
+import { cliente } from "./model/cliente.js";
+import { conta } from "./model/conta.js";
 
+let ender = new endereco(pe, "Recife", "são josé", "rua da guia", 1000, 305, "500-050");
+let agen = new agencia("softex", 1234, "8134546798", "agsoftex@banco.com", endereco);
+let cli = new cliente("claudio", 81987184655);
