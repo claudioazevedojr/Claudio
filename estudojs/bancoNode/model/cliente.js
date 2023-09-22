e4aeed14b27f3a5805323f8d197867962db802c2
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-09-21 22:26:33 -0300

    Update cliente.js

--- estudojs/bancoNode/model/cliente.js
+++ estudojs/bancoNode/model/cliente.js
@@ -1,11 +1,11 @@
-export class cliente {
-    constructor(nome, cpf, datanascimento, rg, email, telefone, end) {
+export class Cliente{
+    constructor(nome, cpf, dataNasc, rg, email, telfone, end){
         this.nome = nome;
         this.cpf = cpf;
-        this.datanascimento = datanascimento;
+        this.dataNasc = dataNasc;
         this.rg = rg;
         this.email = email;
-        this.telefone = telefone;
+        this.telfone = telfone;
         this.end = end;
     }
 }
