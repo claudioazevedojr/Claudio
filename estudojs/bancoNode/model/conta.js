f0d217bb9da8d0a4b1135fa84eba91380dace459
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-09-25 19:43:00 -0300

    Update conta.js

--- estudojs/bancoNode/model/conta.js
+++ estudojs/bancoNode/model/conta.js
@@ -32,9 +32,29 @@ export class Conta {
     }
 
     // transferir da conta
+    transferir(valor, contaFav){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            let trans = new Transacao(TIPOTRANSACAO.transferir, new Date().toLocaleDateString(), valor, contaFav.cliente, '-');
+            this.transacoes.push(trans);
+            contaFav.saldo += valor;
+            let transFav = new Transacao(TIPOTRANSACAO.transferir, new Date().toLocaleDateString(), valor, this.cliente, '+');
+        }else {
+            console.error('Erro: Saldo insuficiente ' + valor + 'maior do que o saldo' + this.saldo + '.');
+        }
+    }
 
     // realizar pagamento da conta
-
+    pagar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            let trans = new Transacao(TIPOTRANSACAO.pagar, new Date().toLocaleDateString(), valor, null, '-');
+            this.transacoes.push(trans);
+        } else {
+            // lançar um erro
+            console.error('Erro: Saldo insuficiente'+ valor +'maior do que o saldo' + this.saldo + '.');
+        }
+    }
     // mostrar saldo da conta
 
     // mostrar extrato da conta
