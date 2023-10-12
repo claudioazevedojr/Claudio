3d19d8473155d30835f4012a38f92194b4b9dba5
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-10-12 17:38:49 -0300

    Update fabricaPC.js

--- estudojs/fabricaPC.js
+++ estudojs/fabricaPC.js
@@ -1,36 +1,36 @@
-class Computer {
-    constructor(ram, hdd, cpu, type) {
+class Computador {
+    constructor(ram, hdd, cpu, Tipo) {
       this.ram = ram;
       this.hdd = hdd;
       this.cpu = cpu;
-      this.type = type;
+      this.Tipo = Tipo;
     }
   
     resultado() {
-      return `Type: ${this.type}, RAM: ${this.ram} GB, HDD: ${this.hdd} GB, CPU: ${this.cpu} GHz`;
+      return `Tipo: ${this.Tipo}, RAM: ${this.ram} GB, HDD: ${this.hdd} GB, CPU: ${this.cpu} GHz`;
     }
   }
   
   
-  class PC extends Computer {
+  class PC extends Computador {
     constructor(ram, hdd, cpu) {
       super(ram, hdd, cpu, 'PC');
     }
   }
   
   
-  class Server extends Computer {
+  class Server extends Computador {
     constructor(ram, hdd, cpu) {
       super(ram, hdd, cpu, 'Server');
     }
   }
   
   
-  class ComputerFactory {
-    createComputer(type, ram, hdd, cpu) {
-      if (type === 'PC') {
+  class montarMaquina {
+    createComputador(Tipo, ram, hdd, cpu) {
+      if (Tipo === 'PC') {
         return new PC(ram, hdd, cpu);
-      } else if (type === 'Server') {
+      } else if (Tipo === 'Server') {
         return new Server(ram, hdd, cpu);
       } else {
         throw new Error('Tipo de computador não disponivel');
@@ -39,9 +39,9 @@ class Computer {
   }
   
   
-  const factory = new ComputerFactory();
-  const pc = factory.createComputer('PC', 8, 512, 2.4);
-  const server = factory.createComputer('Server', 32, 1000, 3.2);
+  const criar = new montarMaquina();
+  const pc = criar.createComputador('PC', 16, 512, 2.8);
+  const server = criar.createComputador('Server', 32, 1000, 3.2);
   
   console.log(pc.resultado());
   console.log(server.resultado());
