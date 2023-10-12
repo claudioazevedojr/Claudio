858521699295f34d0f9d90eba69081dd12330542
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-10-12 17:49:18 -0300

    Update fabricaPC.js

--- estudojs/fabricaPC.js
+++ estudojs/fabricaPC.js
@@ -19,9 +19,9 @@ class Computador {
   }
   
   
-  class Server extends Computador {
+  class Servidor extends Computador {
     constructor(ram, hdd, cpu) {
-      super(ram, hdd, cpu, 'Server');
+      super(ram, hdd, cpu, 'Servidor');
     }
   }
   
@@ -30,8 +30,8 @@ class Computador {
     createComputador(Tipo, ram, hdd, cpu) {
       if (Tipo === 'PC') {
         return new PC(ram, hdd, cpu);
-      } else if (Tipo === 'Server') {
-        return new Server(ram, hdd, cpu);
+      } else if (Tipo === 'Servidor') {
+        return new Servidor(ram, hdd, cpu);
       } else {
         throw new Error('Tipo de computador não disponivel');
       }
@@ -41,8 +41,8 @@ class Computador {
   
   const criar = new montarMaquina();
   const pc = criar.createComputador('PC', 16, 512, 2.8);
-  const server = criar.createComputador('Server', 32, 1000, 3.2);
+  const servidor = criar.createComputador('Servidor', 32, 1000, 3.2);
   
   console.log(pc.resultado());
-  console.log(server.resultado());
+  console.log(servidor.resultado());
   
