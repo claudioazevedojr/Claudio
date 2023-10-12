13801a209f85ce34054423a08289d6cee554f768
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-10-12 12:40:30 -0300

    Create fabricaPC.js

---
+++ estudojs/fabricaPC.js
@@ -0,0 +1,49 @@
+// Interface para os produtos
+class Computer {
+    constructor(ram, hdd, cpu, type) {
+      this.ram = ram;
+      this.hdd = hdd;
+      this.cpu = cpu;
+      this.type = type;
+    }
+  
+    resultado() {
+      return `Type: ${this.type}, RAM: ${this.ram} GB, HDD: ${this.hdd} GB, CPU: ${this.cpu} GHz`;
+    }
+  }
+  
+  // Classe concreta PC
+  class PC extends Computer {
+    constructor(ram, hdd, cpu) {
+      super(ram, hdd, cpu, 'PC');
+    }
+  }
+  
+  // Classe concreta Server
+  class Server extends Computer {
+    constructor(ram, hdd, cpu) {
+      super(ram, hdd, cpu, 'Server');
+    }
+  }
+  
+  // Fábrica para criar instâncias de Computer
+  class ComputerFactory {
+    createComputer(type, ram, hdd, cpu) {
+      if (type === 'PC') {
+        return new PC(ram, hdd, cpu);
+      } else if (type === 'Server') {
+        return new Server(ram, hdd, cpu);
+      } else {
+        throw new Error('Tipo de computador não disponivel');
+      }
+    }
+  }
+  
+  // Exemplo de uso
+  const factory = new ComputerFactory();
+  const pc = factory.createComputer('PC', 8, 512, 2.4);
+  const server = factory.createComputer('Server', 32, 1000, 3.2);
+  
+  console.log(pc.resultado());
+  console.log(server.resultado());
+  
